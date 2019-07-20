4f3596eb68ccaff0336f3cd67f5d63efdc5f005b
Carlos Mertens <mertenscarlos@yahoo.com>
2019-07-20 16:13:59 +0200

    Create hello_world file

---
+++ The-Basics/hello_world.py
@@ -0,0 +1,19 @@
+"""Let's see how all begin"""
+
+# Print a message
+print("Hello, World!")
+
+# Get input from user
+name = input("Hey there, what is your name? ")
+
+# Print a greeting
+greeting = "Welcome"
+print(greeting + " " + name + "!")
+
+# Split over lines
+split_message = "This is how\n you can split\nlines"
+print(split_message)
+
+# Tab characters in a String
+split_numbers = "1\t2\t3\t4\t5"
+print(split_numbers)
